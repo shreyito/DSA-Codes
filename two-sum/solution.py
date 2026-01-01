96d743e82cd79458529a7b49189e71da8d5cfd41
Shreya Raj Gupta <121806806+shreyito@users.noreply.github.com>
2026-01-01 22:09:15 +0530

    Update solution.py

--- two-sum/solution.py
+++ two-sum/solution.py
@@ -3,7 +3,7 @@ def two_sum(nums, target):
     Given an array of integers nums and an integer target,
     return indices of the two numbers such that they add up to target.
 
-    It is guaranteed that there will be exactly one solution,
+    It is guaranteed that there will be exactly one solution, 
     and you may not use the same element twice.
 
     Args:
