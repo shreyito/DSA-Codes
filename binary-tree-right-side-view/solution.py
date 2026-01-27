fef0e895af46ac98a2b40f90efafd1471d93c4fe
Shreya Raj Gupta <121806806+shreyito@users.noreply.github.com>
2026-01-27 23:44:00 +0530

    Update solution.py

--- binary-tree-right-side-view/solution.py
+++ binary-tree-right-side-view/solution.py
@@ -3,7 +3,7 @@ import collections
 
 # Definition for a binary tree node.
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+    def __init__(self, val=0, left=None, right=None): 
         self.val = val
         self.left = left
         self.right = right
